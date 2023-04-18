6b04cc29897c00331c710970e4af4ae0ae5bf501
Sambit Kumar <sambit.kumar142@gmail.com>
2023-04-18 07:47:14 +0530

    configured static files in settings updated url to serve static files locally updated gitignore to ignored static files directory

--- Shoppoholic/Shoppoholic/settings.py
+++ Shoppoholic/Shoppoholic/settings.py
@@ -117,6 +117,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+STATIC_ROOT = BASE_DIR.parent / "static-cdn" / "static-root"
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR.parent / "static-cdn" / "media-root"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- Shoppoholic/Shoppoholic/urls.py
+++ Shoppoholic/Shoppoholic/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import home_page, contact_page, about_page, login_page, register_page
 
@@ -27,3 +29,6 @@ urlpatterns = [
     path("login", login_page),
     path("register", register_page),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
